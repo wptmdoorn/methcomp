9be93a6d6cc549bb7dd1ae573675d74beb6acf90
William van Doorn <wptmdoorn@gmail.com>
2021-05-19 17:55:46 +0200

    Revisions after code revie
    - Revision throughout after code review #7
    - Small update to tests, need further optimization

--- methcomp/blandaltman.py
+++ methcomp/blandaltman.py
@@ -3,19 +3,38 @@ import matplotlib
 import matplotlib.transforms as transforms
 import numpy as np
 from scipy import stats
-from typing import Tuple
+from typing import Tuple, Dict, Any
 
 __all__ = ["blandaltman"]
 
 
-class _BlandAltman(object):
-    """Internal class for drawing a Bland-Altman plot"""
+class BlandAltman:
+    """Class for drawing a Bland-Altman plot"""
 
-    def __init__(self, method1, method2,
-                 diff, limit_of_agreement, CI):
+    DEFAULT_POINTS_KWS = {"s": 20, "alpha": 0.6, "color": "#000000"}
+
+    def __init__(
+        self, method1, method2, diff: str, limit_of_agreement: float, CI: float
+    ):
+        """
+
+        Parameters
+        ----------
+        diff : "absolute"  or "percentage"
+        The difference to display, whether it is an absolute one or a percentual one.
+        If None is provided, it defaults to absolute.
+        limit_of_agreement : float, optional
+        Multiples of the standard deviation to plot the limit of afgreement bounds at.
+        This defaults to 1.96.
+        CI : float, optional
+        The confidence interval employed in the mean difference and limit of agreement
+        lines. Defaults to 0.95.
+        """
+
         # variables assignment
+        self.computed = False
         self.method1: np.array = np.asarray(method1)
-        self.method2: np.array = np.asarray(method2) 
+        self.method2: np.array = np.asarray(method2)
         self.diff_method: str = diff
         self.CI: float = CI
         self.loa: float = limit_of_agreement
@@ -23,14 +42,26 @@ class _BlandAltman(object):
         # check provided parameters
         self._check_params()
 
-    def compute(self) -> dict:
-        # perform necessary calculations and processing
+    def compute(self) -> Dict[str, Any]:
+        """Calculates the statistics for method comparison using
+        Bland-Altman plotting. Returns a dictionary with the results.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        ----------
+        Dict[str, Any] : a dictionary containing the mean and limit of agreement values and
+                         their confidence intervals
+        """
+
         self.n: float = len(self.method1)
         self.mean: np.array = np.mean([self.method1, self.method2], axis=0)
 
-        if self.diff_method == 'absolute':
+        if self.diff_method == "absolute":
             self.diff = self.method1 - self.method2
-        elif self.diff_method == 'percentage':
+        elif self.diff_method == "percentage":
             self.diff = ((self.method1 - self.method2) / self.mean) * 100
         else:
             self.diff = self.method1 - self.method2
@@ -40,87 +71,183 @@ class _BlandAltman(object):
         self.loa_sd = self.loa * self.sd_diff
 
         if self.CI is not None:
-            self.CI_mean = stats.norm.interval(alpha=self.CI, loc=self.mean_diff,
-                                               scale=self.sd_diff / np.sqrt(self.n))
-            se_loa = (1.71 ** 2) * ((self.sd_diff**2) / self.n)
-            conf_loa = np.sqrt(se_loa) * stats.t.ppf(q=(1 - self.CI) / 2., df=self.n - 1)
-            self.CI_upper = [self.mean_diff + self.loa_sd + conf_loa,
-                             self.mean_diff + self.loa_sd - conf_loa]
-            self.CI_lower = [self.mean_diff - self.loa_sd + conf_loa,
-                             self.mean_diff - self.loa_sd - conf_loa]
+            self.CI_mean = stats.norm.interval(
+                alpha=self.CI, loc=self.mean_diff, scale=self.sd_diff / np.sqrt(self.n)
+            )
+            se_loa = (1.71 ** 2) * ((self.sd_diff ** 2) / self.n)
+            conf_loa = np.sqrt(se_loa) * stats.t.ppf(
+                q=(1 - self.CI) / 2.0, df=self.n - 1
+            )
+            self.CI_upper = [
+                self.mean_diff + self.loa_sd + conf_loa,
+                self.mean_diff + self.loa_sd - conf_loa,
+            ]
+            self.CI_lower = [
+                self.mean_diff - self.loa_sd + conf_loa,
+                self.mean_diff - self.loa_sd - conf_loa,
+            ]
 
-        self._output = {'mean': self.mean_diff,
-                       'mean_CI': self.CI_mean if self.CI else None, 
-                       'loa_lower': self.mean_diff - self.loa_sd,
-                       'loa_lower_CI': self.CI_lower if self.CI else None,
-                       'loa_upper': self.mean_diff + self.loa_sd,
-                       'loa_upper_CI': self.CI_upper if self.CI else None,
-                       }
-        
+        self._output = {
+            "mean": self.mean_diff,
+            "mean_CI": self.CI_mean if self.CI else None,
+            "loa_lower": self.mean_diff - self.loa_sd,
+            "loa_lower_CI": self.CI_lower if self.CI else None,
+            "loa_upper": self.mean_diff + self.loa_sd,
+            "loa_upper_CI": self.CI_upper if self.CI else None,
+        }
+
+        self.computed = True
+
         return self._output
 
     def _check_params(self):
         if len(self.method1) != len(self.method2):
-            raise ValueError('Length of method 1 and method 2 are not equal.')
+            raise ValueError("Length of method 1 and method 2 are not equal.")
 
         if self.CI is not None and (self.CI > 1 or self.CI < 0):
-            raise ValueError('Confidence interval must be between 0 and 1.')
+            raise ValueError("Confidence interval must be between 0 and 1.")
 
-        if self.diff_method not in ['absolute', 'percentage']:
-            raise ValueError('The provided difference method must be either absolute or percentage.')
+        if self.diff_method not in ["absolute", "percentage"]:
+            raise ValueError(
+                "The provided difference method must be either absolute or percentage."
+            )
 
-        #if any([not isinstance(x, str) for x in [self.x_title, self.y_title]]):
+        # if any([not isinstance(x, str) for x in [self.x_title, self.y_title]]):
         #    raise ValueError('Axes labels arguments should be provided as a str.')
-        
-        
-    def plot(self,
-             x_title='Mean of methods', y_title='Difference between methods', graph_title=None,
-             reference=False, xlim=None, ylim=None,
-             color_mean='#008bff', color_loa='#FF7000', color_points='#000000', point_kws=None,
-             ax: matplotlib.axes.Axes=None):
-        
-        if not(hasattr(self, '_output')):
+
+    def plot(
+        self,
+        x_label: str = "Mean of methods",
+        y_label: str = "Difference between methods",
+        graph_title: str = None,
+        reference: bool = False,
+        xlim: Tuple = None,
+        ylim: Tuple = None,
+        color_mean: int = "#008bff",
+        color_loa: int = "#FF7000",
+        color_points: int = "#000000",
+        point_kws: Dict = None,
+        ax: matplotlib.axes.Axes = None,
+    ):
+        """Provide a method comparison using Bland-Altman plotting.
+        This is an Axis-level function which will draw the Bland-Altman plot
+        onto the current active Axis object unless ``ax`` is provided.
+        Parameters
+        ----------
+        x_label : str, optional
+            The label which is added to the X-axis. If None is provided, a standard
+            label will be added.
+        y_label : str, optional
+            The label which is added to the Y-axis. If None is provided, a standard
+            label will be added.
+        graph_title : str, optional
+            Title of the Bland-Altman plot. If None is provided, no title will be plotted.
+        reference : bool, optional
+            If True, a grey reference line at y=0 will be plotted in the Bland-Altman.
+        xlim : list, optional
+            Minimum and maximum limits for X-axis. Should be provided as list or tuple.
+            If not set, matplotlib will decide its own bounds.
+        ylim : list, optional
+            Minimum and maximum limits for Y-axis. Should be provided as list or tuple.
+            If not set, matplotlib will decide its own bounds.
+        color_mean : str, optional
+            Color of the mean difference line that will be plotted.
+        color_loa : str, optional
+            Color of the limit of agreement lines that will be plotted.
+        color_points : str, optional
+            Color of the individual differences that will be plotted.
+        point_kws : dict of key, value mappings, optional
+            Additional keyword arguments for `plt.scatter`.
+        ax : matplotlib Axes, optional
+            Axes in which to draw the plot, otherwise use the currently-active
+            Axes.
+        Returns
+        -------
+        ax : matplotlib Axes
+            Axes object with the Bland-Altman plot.
+        """
+
+        if not (self.computed):
             self.compute()
-        
-        point_kws: dict = {} if point_kws is None else point_kws.copy()
-        ax = plt.gca()
-        
+
+        pkws = self.DEFAULT_POINTS_KWS.copy()
+        pkws.update(point_kws or {})
+        ax = ax or plt.gca()
+
         # individual points
-        ax.scatter(self.mean, self.diff, s=20, alpha=0.6, color=color_points,
-                   **point_kws)
+        ax.scatter(self.mean, self.diff, **pkws)
 
         # mean difference and SD lines
-        ax.axhline(self.mean_diff, color=color_mean, linestyle='-')
-        ax.axhline(self.mean_diff + self.loa_sd, color=color_loa, linestyle='--')
-        ax.axhline(self.mean_diff - self.loa_sd, color=color_loa, linestyle='--')
+        ax.axhline(self.mean_diff, color=color_mean, linestyle="-")
+        ax.axhline(self.mean_diff + self.loa_sd, color=color_loa, linestyle="--")
+        ax.axhline(self.mean_diff - self.loa_sd, color=color_loa, linestyle="--")
 
         if reference:
-            ax.axhline(0, color='grey', linestyle='-', alpha=0.4)
+            ax.axhline(0, color="grey", linestyle="-", alpha=0.4)
 
         # confidence intervals (if requested)
         if self.CI is not None:
-            ax.axhspan(self.CI_mean[0],  self.CI_mean[1], color=color_mean, alpha=0.2)
+            ax.axhspan(self.CI_mean[0], self.CI_mean[1], color=color_mean, alpha=0.2)
             ax.axhspan(self.CI_upper[0], self.CI_upper[1], color=color_loa, alpha=0.2)
             ax.axhspan(self.CI_lower[0], self.CI_lower[1], color=color_loa, alpha=0.2)
 
         # text in graph
         trans: matplotlib.transform = transforms.blended_transform_factory(
-            ax.transAxes, ax.transData)
+            ax.transAxes, ax.transData
+        )
         offset: float = (((self.loa * self.sd_diff) * 2) / 100) * 1.2
-        ax.text(0.98, self.mean_diff + offset, 'Mean', ha="right", va="bottom", transform=trans)
-        ax.text(0.98, self.mean_diff - offset, f'{self.mean_diff:.2f}', ha="right", va="top", transform=trans)
-        ax.text(0.98, self.mean_diff + self.loa_sd + offset,
-                f'+{self.loa:.2f} SD', ha="right", va="bottom", transform=trans)
-        ax.text(0.98, self.mean_diff + self.loa_sd - offset,
-                f'{self.mean_diff + self.loa_sd:.2f}', ha="right", va="top", transform=trans)
-        ax.text(0.98, self.mean_diff - self.loa_sd - offset,
-                f'-{self.loa:.2f} SD', ha="right", va="top", transform=trans)
-        ax.text(0.98, self.mean_diff - self.loa_sd + offset,
-                f'{self.mean_diff - self.loa_sd:.2f}', ha="right", va="bottom", transform=trans)
+        ax.text(
+            0.98,
+            self.mean_diff + offset,
+            "Mean",
+            ha="right",
+            va="bottom",
+            transform=trans,
+        )
+        ax.text(
+            0.98,
+            self.mean_diff - offset,
+            f"{self.mean_diff:.2f}",
+            ha="right",
+            va="top",
+            transform=trans,
+        )
+        ax.text(
+            0.98,
+            self.mean_diff + self.loa_sd + offset,
+            f"+{self.loa:.2f} SD",
+            ha="right",
+            va="bottom",
+            transform=trans,
+        )
+        ax.text(
+            0.98,
+            self.mean_diff + self.loa_sd - offset,
+            f"{self.mean_diff + self.loa_sd:.2f}",
+            ha="right",
+            va="top",
+            transform=trans,
+        )
+        ax.text(
+            0.98,
+            self.mean_diff - self.loa_sd - offset,
+            f"-{self.loa:.2f} SD",
+            ha="right",
+            va="top",
+            transform=trans,
+        )
+        ax.text(
+            0.98,
+            self.mean_diff - self.loa_sd + offset,
+            f"{self.mean_diff - self.loa_sd:.2f}",
+            ha="right",
+            va="bottom",
+            transform=trans,
+        )
 
         # transform graphs
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["top"].set_visible(False)
 
         # set X and Y limits
         if xlim is not None:
@@ -129,15 +256,15 @@ class _BlandAltman(object):
             ax.set_ylim(ylim[0], ylim[1])
 
         # graph labels
-        ax.set_ylabel(y_title)
-        ax.set_xlabel(x_title)
-        if graph_title is not None:
-            ax.set_title(graph_title)
+        ax.set(xlabel=x_label, ylabel=y_label, title=graph_title)
+
+        return ax
 
 
-def blandaltman(method1, method2,
-                diff='absolute', limit_of_agreement=1.96, CI=0.95) -> _BlandAltman:
-    """Provide a method comparison using Bland-Altman. 
+def blandaltman(
+    method1, method2, diff="absolute", limit_of_agreement=1.96, CI=0.95
+) -> BlandAltman:
+    """Provide a method comparison using Bland-Altman.
 
     This functions creates a BlandAltman class which can be used to
     access the statistics (using .statistics()) or to generate a plot
@@ -160,7 +287,7 @@ def blandaltman(method1, method2,
 
     Returns
     -------
-    _BlandAltman : class object containing the statistics and plot functionality
+    BlandAltman : class object containing the statistics and plot functionality
 
     See Also
     -------
@@ -170,7 +297,4 @@ def blandaltman(method1, method2,
 
     """
 
-    ba: _BlandAltman = _BlandAltman(method1, method2, diff, limit_of_agreement, CI)
-
-    # Return BlandAltman
-    return ba
+    return BlandAltman(method1, method2, diff, limit_of_agreement, CI)
--- methcomp/tests/test_blandaltman.py
+++ methcomp/tests/test_blandaltman.py
@@ -1,13 +1,64 @@
 from methcomp import blandaltman
 import matplotlib.pyplot as plt
 import pytest
+import numpy as np
 
-method1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-method2 = [1.03, 2.05, 2.79, 3.67,
-           5.00, 5.82, 7.16, 7.69,
-           8.53, 10.38, 11.11, 12.17,
-           13.47, 13.83, 15.15, 16.12,
-           16.94, 18.09, 19.13, 19.54]
+
+@pytest.fixture
+def method1():
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
+@pytest.fixture
+def method2():
+    return [
+        1.03,
+        2.05,
+        2.79,
+        3.67,
+        5.00,
+        5.82,
+        7.16,
+        7.69,
+        8.53,
+        10.38,
+        11.11,
+        12.17,
+        13.47,
+        13.83,
+        15.15,
+        16.12,
+        16.94,
+        18.09,
+        19.13,
+        19.54,
+    ]
+
+
+@pytest.mark.parametrize(
+    "CI, m, mlo, mhi, l, llo, lhi, h, hlo, hhi",
+    [
+        (
+            0.95,
+            0.016499999999,
+            -0.092549364,
+            0.1255493648768,
+            -0.471192547000,
+            -0.6703265295,
+            -0.2720585644,
+            0.50419254700,
+            0.3050585644,
+            0.7033265295,
+        ),
+    ],
+)
+def test_blandaltman_compute(
+    method1, method2, CI, m, mlo, mhi, l, llo, lhi, h, hlo, hhi
+):
+    results = blandaltman(method1, method2, CI=CI).compute()
+    # Expected
+    np.testing.assert_allclose(results["mean"], m, rtol=1e-5)
+    np.testing.assert_allclose(results["mean_CI"], (mlo, mhi), rtol=1e-5)
 
 
 @pytest.mark.mpl_image_compare(tolerance=10)
@@ -20,7 +71,7 @@ def test_blandaltman_basic():
 @pytest.mark.mpl_image_compare(tolerance=10)
 def test_blandaltman_basic_title():
     fig, ax = plt.subplots(1, 1)
-    blandaltman(method1, method2, title='Test', ax=ax)
+    blandaltman(method1, method2, title="Test", ax=ax)
     return fig
 
 
@@ -34,5 +85,5 @@ def test_blandaltman_basic_without_CI():
 @pytest.mark.mpl_image_compare(tolerance=10)
 def test_blandaltman_basic_percentage():
     fig, ax = plt.subplots(1, 1)
-    blandaltman(method1, method2, diff='percentage', ax=ax)
+    blandaltman(method1, method2, diff="percentage", ax=ax)
     return fig
